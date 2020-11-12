329ab1666656f760da6a555c51dda668fc23cab4
Thomas Gilbert <tcgilbert94@gmail.com>
2020-11-12 15:01:33 -0500

    add myModule.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const { add, minus } = require("./myModule");
+
 let name = "Thomas Gilbert"
-console.log(name);
 
 function printName(person) {
     return "hello " + person;
 }
 
-console.log(printName(name));
+console.log(add(5, 23));
+
+console.log(minus(6, 9));
+
+console.log(beBasic());
---
+++ myModule.js
@@ -0,0 +1,15 @@
+const beBasic = () => "That's so fetch!";
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function minus(num1, num2) {
+    return num1 - num2;
+}
+
+module.exports = {
+    beBasic, 
+    add, 
+    minus
+}
